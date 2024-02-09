fc8512c097ab09d1403f214141b2a06e60569738
Harvey Olden <2733714O@student.gla.ac.uk>
2024-02-09 10:59:01 -0300

    Commit 12

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 from django.shortcuts import render
 
 from django.http import HttpResponse
+
+from django.contrib.auth.decorators import login_required
 
 from rango.models import Category
 from rango.models import Page
@@ -46,6 +48,7 @@ def show_category(request, category_name_slug):
         
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -58,6 +61,7 @@ def add_category(request):
         print(form.errors)
         return render(request, 'rango/add_category.html', {'form': form})
     
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -132,3 +136,12 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rango/login.html')
+    
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
